15ecb3e0c6f63d08726bf44c72adc2b35fcb831f
Cristian HF <cristian.xwk@gmail.com>
2018-04-11 20:21:40 +0200

    commt
    commit final

--- src/main/java/com/agenda/grupocuatro/services/PersonasService.java
+++ src/main/java/com/agenda/grupocuatro/services/PersonasService.java
@@ -1,5 +1,13 @@
 package com.agenda.grupocuatro.services;
 
+import java.util.List;
+
+import com.agenda.grupocuatro.model.Personas;
+
 public interface PersonasService {
 
+	public List<Personas> listaPersonas();
+	
+	public void altaOupdate(Personas persona);
+	
 }
