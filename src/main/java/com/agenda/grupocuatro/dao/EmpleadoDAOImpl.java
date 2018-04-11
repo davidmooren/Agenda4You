5d83566a87111f6b54eb78c25edca9b4646f6660
OlgaMaestre <olgamm16@gmail.com>
2018-04-11 17:59:05 +0200

    commit
    empleados

--- src/main/java/com/agenda/grupocuatro/dao/EmpleadoDAOImpl.java
+++ src/main/java/com/agenda/grupocuatro/dao/EmpleadoDAOImpl.java
@@ -7,11 +7,13 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.agenda.grupocuatro.model.Categorias;
 import com.agenda.grupocuatro.model.Empleado;
 
 @Repository
 public class EmpleadoDAOImpl implements EmpleadoDAO{
 
+	
 	@Autowired
 	private SessionFactory sessionFactory;
 	
@@ -44,12 +46,15 @@ public class EmpleadoDAOImpl implements EmpleadoDAO{
 	public void altaOupdate(Empleado empleado) {
 		// TODO Auto-generated method stub
 		
+		sessionFactory.getCurrentSession().saveOrUpdate(empleado);
 	}
 
 	@Override
 	public void baja(int idEmpleado) {
 		// TODO Auto-generated method stub
-		
+		Empleado empleadoABorrar = new Empleado();
+		empleadoABorrar.setIdempleados(idEmpleado);
+		sessionFactory.getCurrentSession().delete(empleadoABorrar);
 	}
 
 }
