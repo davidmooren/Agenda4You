20fce7613c0a7e33011a823cf279b3d4a03083b7
DaniDauron <37369310+DaniDauron@users.noreply.github.com>
2018-04-12 08:57:58 +0200

    commit
    muchos cambios

--- src/main/java/com/agenda/grupocuatro/config/ApplicationContextConfig.java
+++ src/main/java/com/agenda/grupocuatro/config/ApplicationContextConfig.java
@@ -1,19 +1,41 @@
 package com.agenda.grupocuatro.config;
 
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.agenda.grupocuatro.model.Categorias;
@@ -23,9 +45,10 @@ import com.agenda.grupocuatro.model.Empleado;
 import com.agenda.grupocuatro.model.UsuarioAdmin;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("com.agenda.grupocuatro")
 @EnableTransactionManagement
-public class ApplicationContextConfig {
+public class ApplicationContextConfig implements WebMvcConfigurer{
     @Bean(name = "viewResolver")
     public InternalResourceViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -34,15 +57,15 @@ public class ApplicationContextConfig {
         return viewResolver;
     }
     
-
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-		///registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");	    
-	}
+ 
      
     
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
+    	
+    	Logger logeer = Logger.getLogger("DataSource");
+    	logeer.log(Level.WARN, "paso por resolver");
+    	
     	BasicDataSource dataSource = new BasicDataSource();
     	dataSource.setDriverClassName("com.mysql.jdbc.Driver");
     	dataSource.setUrl("jdbc:mysql://192.168.200.17:3306/agenda");
@@ -81,6 +104,139 @@ public class ApplicationContextConfig {
 				sessionFactory);
 
 		return transactionManager;
+	}
+
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addFormatters(FormatterRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addInterceptors(InterceptorRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {	
+	    	Logger logeer = Logger.getLogger("Resolverrrrrrrr");
+	    	logeer.log(Level.WARN, "paso por resolver");
+		//    registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+			///registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");	   
+		    registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/statics/", "D:/statics/")
+            .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+		
 	}
     
 
--- src/main/webapp/WEB-INF/component/imports.jsp
+++ src/main/webapp/WEB-INF/component/imports.jsp
@@ -1,8 +1,15 @@
+ <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+ 
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <meta charset="UTF-8">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
- <link rel="stylesheet" href="./css/propio.css">
+ <link rel="stylesheet" href="./css/propio.css"> 
+ 
+<link href='<spring:url value="/resources/css/custom.css"/>' rel="stylesheet" />
+
  
