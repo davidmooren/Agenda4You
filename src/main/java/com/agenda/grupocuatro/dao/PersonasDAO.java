4d49177e4a039939ef700c07f1892a086b677c52
DaniDauron <37369310+DaniDauron@users.noreply.github.com>
2018-04-12 09:31:16 +0200

    commit
    personas

--- src/main/java/com/agenda/grupocuatro/dao/PersonasDAO.java
+++ src/main/java/com/agenda/grupocuatro/dao/PersonasDAO.java
@@ -1,14 +1,14 @@
-package com.agenda.grupocuatro.services;
+package com.agenda.grupocuatro.dao;
 
 import java.util.List;
+
 import com.agenda.grupocuatro.model.Personas;
 
-public interface PersonasService {
+
+public interface PersonasDAO {
 
 	public Personas get(int id);
 	public List<Personas> listaPersonas();	
 	public void altaOupdate(Personas persona);
 	public	void baja(int id);
-	
-	
 }
