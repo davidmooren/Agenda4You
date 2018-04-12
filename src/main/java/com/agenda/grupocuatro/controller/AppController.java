4ed6eae2f6c135998b0796695dd29ed3a9854edb
DaniDauron <37369310+DaniDauron@users.noreply.github.com>
2018-04-12 09:38:57 +0200

    controler
    personas

--- src/main/java/com/agenda/grupocuatro/controller/AppController.java
+++ src/main/java/com/agenda/grupocuatro/controller/AppController.java
@@ -18,11 +18,13 @@ import com.agenda.grupocuatro.model.Categorias;
 import com.agenda.grupocuatro.model.Contact;
 import com.agenda.grupocuatro.model.Departamento;
 import com.agenda.grupocuatro.model.Empleado;
+import com.agenda.grupocuatro.model.Personas;
 import com.agenda.grupocuatro.model.UsuarioAdmin;
 import com.agenda.grupocuatro.services.CategoriasService;
 import com.agenda.grupocuatro.services.ContactService;
 import com.agenda.grupocuatro.services.DepartamentoService;
 import com.agenda.grupocuatro.services.EmpleadoService;
+import com.agenda.grupocuatro.services.PersonasService;
 import com.agenda.grupocuatro.services.UsuarioAdminService;
 
 /**
@@ -41,7 +43,8 @@ public class AppController {
 	private CategoriasService categoriaService;
 	@Autowired
 	private DepartamentoService departamentoService;
-
+	@Autowired
+	private PersonasService personasService;
 	/*@RequestMapping("/")
 	public ModelAndView handleRequest() throws Exception {
 		ModelAndView model = new ModelAndView("login");
@@ -147,10 +150,7 @@ public class AppController {
 		empleadoService.altaOupdate(empleado);
 		return new ModelAndView("redirect:/");
 	}
-	
-	
-	
-	
+		
 	/*Fin empleados*/
 	
 	
@@ -163,7 +163,7 @@ public class AppController {
 		return model;		
 	}
 	
-	
+	/*Inicio Categoria*/
 	
 	@RequestMapping(value = "/newCategoria", method = RequestMethod.GET)
 	public ModelAndView newCategoria() {
@@ -201,7 +201,9 @@ public class AppController {
 		categoriaService.altaOupdate(categoria);
 		return new ModelAndView("redirect:/listCategorias");
 	}
+	/*Fin categoria*/
 	
+
 	
 	
 	@RequestMapping(value ="/newDepartamento", method=RequestMethod.GET)
@@ -241,4 +243,45 @@ public class AppController {
 		return new ModelAndView("redirect:/listDepartamentos");
 	}
 	
+	
+	/*Inicio Personas*/
+/*Inicio Categoria*/
+	
+	@RequestMapping(value = "/newPersona", method = RequestMethod.GET)
+	public ModelAndView newPersona() {
+		ModelAndView model = new ModelAndView("personaform");
+		model.addObject("persona", new Personas());
+		return model;		
+	}
+	
+	@RequestMapping(value = "/editPersona", method = RequestMethod.GET)
+	public ModelAndView editPersona(HttpServletRequest request) {
+		int personaId = Integer.parseInt(request.getParameter("id"));
+		Personas persona = personasService.get(personaId);
+		ModelAndView model = new ModelAndView("personaform");
+		model.addObject("persona", persona);
+		return model;		
+	}
+	
+	@RequestMapping(value = "/deletePersona", method = RequestMethod.GET)
+	public ModelAndView deletePersona(HttpServletRequest request) {
+		int personaId = Integer.parseInt(request.getParameter("id"));
+		personasService.baja(personaId);
+		return new ModelAndView("redirect:/listPersonas");		
+	}
+	
+	@RequestMapping(value="/listPersona", method=RequestMethod.GET)
+	public ModelAndView listarPersona(){
+		List<Personas> listaPersonas = personasService.listaPersonas();
+		ModelAndView model = new ModelAndView("listadopersonas");
+		model.addObject("listaPersonas", listaPersonas);
+		return model;
+	}
+	
+	@RequestMapping(value = "/savePersona", method = RequestMethod.POST)
+	public ModelAndView savePersona(@ModelAttribute Personas persona) {
+		personasService.altaOupdate(persona);
+		return new ModelAndView("redirect:/listPersonas");
+	}
+	/*Fin Pesonas*/
 }
