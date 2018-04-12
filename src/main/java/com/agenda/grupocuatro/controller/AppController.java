3c0bd465b98b318d16284073505daa1015096724
OlgaMaestre <olgamm16@gmail.com>
2018-04-12 16:30:24 +0200

    commit
    index

--- src/main/java/com/agenda/grupocuatro/controller/AppController.java
+++ src/main/java/com/agenda/grupocuatro/controller/AppController.java
@@ -54,7 +54,33 @@ public class AppController {
 	
 	@Autowired
 	private PersonasService personasService;
-
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView principal() {
+		ModelAndView model = new ModelAndView("index");
+	
+		/*empleados*/
+		 
+		  List<Empleado> listaEmpleaditos = empleadoService.listaEmpleados();
+		
+		  	model.addObject("listaEmpleaditos", listaEmpleaditos);
+			
+		/*departamento*/
+		 List<Departamento> listaDepartamentitos = departamentoService.listaDepartamentos();
+			
+		 	model.addObject("listaDepartamentitos", listaDepartamentitos);
+			
+		
+		/*categoria*/
+		List<Categorias> listaCategories = categoriaService.listaCaterogias();
+			
+			model.addObject("listaCategories", listaCategories);
+			
+		
+		return model;		
+	}
+	
 	/*@RequestMapping("/")
 	 * 
 	public ModelAndView handleRequest() throws Exception {
@@ -123,7 +149,7 @@ public class AppController {
 	
 	
 	/*Inicio empleados*/
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/listEmpleados", method = RequestMethod.GET)
 	public ModelAndView listaEmpleados(HttpServletRequest request) {		
 		
 		List<Empleado> empleados = empleadoService.listaEmpleados();
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,26 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+
+
+<html>
+<head>
+<meta charset="UTF-8">
+<jsp:include page="../component/imports.jsp" />
+
+<title>Agenda4You</title>
+</head>
+<body>
+
+	<jsp:include page="../component/header.jsp" />
+
+	<div class="container contenedorprincipal">
+		<div class="row">
+
+			
+		</div>
+
+	</div>
+	<jsp:include page="../component/footer.jsp" /></body>
+</html>
